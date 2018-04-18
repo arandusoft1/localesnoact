29b2e473ae57606a06759c113c7eeeb01d203db0
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-18 18:27:19 -0300

    prueba style

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ bottle.debug(True)
 @route('/')
 def index():
 	leer = json.loads(open('locales.json').read())	
-	tabla = " <table style=" + '"width:100%" background-color: #f1f1c1 '+ "> <tr> <th>Firstname</th> <th>Lastname</th> <th>Age</th> </tr> <tr> <td>Jill</td> <td>Smith</td> <td>50</td> </tr> </table> "
+	tabla = "<style> table, th, td { border: 1px solid black; border-collapse: collapse; } th, td { padding: 5px; } th { text-align: left; } </style> <table style=" + '"width:100%"'+ "> <tr> <th>Firstname</th> <th>Lastname</th> <th>Age</th> </tr> <tr> <td>Jill</td> <td>Smith</td> <td>50</td> </tr> </table> "
 	
 		
 	return tabla
