47d1a903cbfe27fcdee7065f2fc1c717a641725c
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-18 18:17:42 -0300

    arreglo de comillas

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ bottle.debug(True)
 @route('/')
 def index():
 	leer = json.loads(open('locales.json').read())	
-	tabla = " <h2>Styling Tables</h2> <h2>Styling Tables</h2> "
+	tabla = " <table style=" + '"width:100%"'+ "> <tr> <th>Firstname</th> <th>Lastname</th> <th>Age</th> </tr> <tr> <td>Jill</td> <td>Smith</td> <td>50</td> </tr> </table> "
 	
 		
 	return tabla
