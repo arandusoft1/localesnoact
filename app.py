15ba44999c211f7b4c5f6ef6416a50a115951a17
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-18 12:14:20 -0300

    arreglo de elto

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def index():
 	tabla = "|   Nombre local   |       Fecha de Vigencia         |  Cantidad de Producto  |"
 	
 	for elemento in leer:
-		tabla = tabla + "\n" + "|" + elemento['NomLocal'] + "  "*(15-len(elemento['NomLocal'])) + "|" + elemento['fVigencia'] + "  "*(30-len(elemento['NomLocal'])) + "|" + elemento['NomLocal'] + "  "*(21-len(str(elemento['CantidadPro']))) + "|"
+		tabla = tabla + "\n" + "|" + elemento['NomLocal'] + "  "*(15-len(elemento['NomLocal'])) + "|" + elemento['fVigencia'] + "  "*(30-len(elemento['fVigencia'])) + "|" + elemento['CantidadPro'] + "  "*(21-len(str(elemento['CantidadPro']))) + "|"
 			
 	return tabla
 
