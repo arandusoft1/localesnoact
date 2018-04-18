aafa2392edc5b8d61e61dc032f6b7e5abfe06717
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-18 18:53:21 -0300

    agregado de json

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ bottle.debug(True)
 @route('/')
 def index():
 	leer = json.loads(open('locales.json').read())	
-	tabla = "<style> table, th, td { border: 1px solid black; border-collapse: collapse; } th, td { padding: 5px; text-align: left; background-color: #f1f1c1;} </style> <table style=" + '"width:100%"'+ "> <tr> <th>Firstname</th> <th>Lastname</th> <th>Age</th> </tr> <tr> <td>Jill</td> <td>Smith</td> <td>50</td> </tr> </table> "
+	tabla = "<style> table, th, td { border: 1px solid black; border-collapse: collapse; } th, td { padding: 5px; text-align: left; background-color: #f1f1c1;} </style> <table style=" + '"width:100%"'+ "> <tr> <th>Empresa</th> <th>Sucursal</th> <th>Fecha de Vigencia</th> <th>Cantidad de Precios</th> </tr>"
 	
+	for elemento in leer:
+		tabla = tabla + "<tr> <td>"+ elemento["Empresa"] +"</td> <td>"+elemento["Sucursal"]+"</td> <td>"+elemento["fVigencia"]+"</td> <td>"+ str(elemento["CantPrecio"])+" </td> </tr> "
 		
+	tabla = tabla +  </table>
+			
 	return tabla
 
 if __name__ == '__main__':
