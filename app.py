0a80fd5faff5427af002608ea6c6890ed8cd212c
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-18 18:10:22 -0300

    h2 h2

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ bottle.debug(True)
 @route('/')
 def index():
 	leer = json.loads(open('locales.json').read())	
-	tabla = " <h2>Styling Tables</h2> "
+	tabla = " <h2>Styling Tables</h2> <h2>Styling Tables</h2> "
 	
 		
 	return tabla
