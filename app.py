2443920b51071a712ecea6769be4856606595cb4
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-18 12:04:20 -0300

    con tabla en json

--- app.py
+++ app.py
@@ -11,7 +11,12 @@ bottle.debug(True)
 @route('/')
 def index():
 	leer = json.loads(open('locales.json').read())	
-	return leer
+	tabla = "|   Nombre local   |       Fecha de Vigencia         |  Cantidad de Producto  |"
+	
+	for elemento in leer:
+		tabla = tabla + "\n" + "|" + elemento['NomLocal'] + "  "*(15-len(elemento['NomLocal'])) + "|" + elemento['fVigencia'] + "  "*(30-len(elemento['NomLocal'])) + "|" + elemento['NomLocal'] + "  "*(21-len(str(elemento['CantidadPro']))) + "|"
+			
+	return tabla
 
 if __name__ == '__main__':
 	run(host='0.0.0.0',port=argv[1])
