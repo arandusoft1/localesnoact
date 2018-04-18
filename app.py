acac521e9d66ae8dd74afc9237497f9371bf8111
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-18 13:00:51 -0300

    replazo de \n a &nbsp;

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def index():
 	tabla = "|   Nombre local   |       Fecha de Vigencia         |  Cantidad de Producto  |"
 	
 	for elemento in leer:
-		tabla = tabla + '<br />\n' + "| " + elemento["NomLocal"] + "  "*(11-len(elemento["NomLocal"])) + "| " + elemento["fVigencia"] + "  "*(30-len(elemento["fVigencia"])) + "| " + str(elemento["CantidadPro"]) + "  "*(21-len(str(elemento["CantidadPro"]))) + "|"
+		tabla = tabla + '<br />\n' + "|&nbsp;" + elemento["NomLocal"] + "&nbsp;"*(15-len(elemento["NomLocal"])) + "|&nbsp;" + elemento["fVigencia"] + "&nbsp;"*(30-len(elemento["fVigencia"])) + "|&nbsp;" + str(elemento["CantidadPro"]) + "&nbsp;"*(21-len(str(elemento["CantidadPro"]))) + "|"
 			
 	return tabla
 
