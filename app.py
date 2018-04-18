2248f159cacd145b1038f0a633e1702d045fbe7a
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-18 18:00:18 -0300

    utima

--- app.py
+++ app.py
@@ -11,37 +11,7 @@ bottle.debug(True)
 @route('/')
 def index():
 	leer = json.loads(open('locales.json').read())	
-	tabla = "
-<body>
-
-<h2>Collapsed Borders</h2>
-<p>If you want the borders to collapse into one border, add the CSS border-collapse property.</p>
-
-<table style="width:100%">
-  <tr>
-    <th>Firstname</th>
-    <th>Lastname</th> 
-    <th>Age</th>
-  </tr>
-  <tr>
-    <td>Jill</td>
-    <td>Smith</td>
-    <td>50</td>
-  </tr>
-  <tr>
-    <td>Eve</td>
-    <td>Jackson</td>
-    <td>94</td>
-  </tr>
-  <tr>
-    <td>John</td>
-    <td>Doe</td>
-    <td>80</td>
-  </tr>
-</table>
-
-</body>
-"
+	tabla = " <body> <h2>Collapsed Borders</h2> <p>If you want the borders to collapse into one border, add the CSS border-collapse property.</p> <table style="width:100%">   <tr>     <th>Firstname</th>     <th>Lastname</th>      <th>Age</th>   </tr>   <tr>     <td>Jill</td>     <td>Smith</td>     <td>50</td>   </tr>   <tr> <td>Eve</td>     <td>Jackson</td>     <td>94</td>   </tr>   <tr>     <td>John</td>     <td>Doe</td>     <td>80</td>  </tr> </table> </body> "
 	
 		
 	return tabla
