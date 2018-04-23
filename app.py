92ee3c68177f83a7cd2a336aa0b0a4c06a9d6841
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-23 12:50:03 -0300

    print rows sin comentarios

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def index():
 	rows = con.fetchall()
 	#empresas= []
 	
-	#for row in rows:
+	for row in rows:
 		# empresas.append({"Empresa": row[1],"Sucursal": row[2],"fVigencia": row[3],"CantPrecio": row[4]})
-	#	print rows
+		print row
 	
 	#leer = {"Empresas": [ empresas ], "UltAct": [{"fVigencia": "05/03/18 00:00:00"}]}
 	
