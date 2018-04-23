eb291601d6ed0ee51dbb261a6f7e6b8b8014d94b
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-23 12:19:47 -0300

    arreglado para postgresql

--- app.py
+++ app.py
@@ -3,15 +3,30 @@
 
 from sys import argv
 import bottle
-import json
+# import json
 from bottle import route,run,Bottle,template
 from datetime import datetime
+import psycopg2, psycopg2.extras
 
 bottle.debug(True)
 
 @route('/')
 def index():
-	leer = json.loads(open('locales.json').read())	
+	conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
+	con = conn.cursor()
+	con.execute("select * from Empresas")
+	rows = con.fetchall()
+	empresas= []
+	
+	for row in rows:
+		empresas.append({"Empresa": row[1],"Sucursal": row[2],"fVigencia": row[3],"CantPrecio": row[4]})
+		
+	print empresas
+	
+	leer = {"Empresas": [ empresas ], "UltAct": [{"fVigencia": "05/03/18 00:00:00"}]}
+	
+		
+	# leer = json.loads(open('locales.json').read())	
 	return template('tabla.tpl', leer)
 
 if __name__ == '__main__':
