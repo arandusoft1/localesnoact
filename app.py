c843fcdcd2fc2370c3fd3df8b5890e4d6542e4f6
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-23 18:18:47 -0300

    correcion row 3 y dias

--- app.py
+++ app.py
@@ -21,13 +21,13 @@ def index():
 	
 	for row in rows:
 		empresas.append({"Empresa": row[1],"Sucursal": row[2],"fVigencia": row[3],"CantPrecio": row[4]})
-		d1 = datetime.strptime(row[2],fmt)		
+		d1 = datetime.strptime(row[3],fmt)		
 		diffhora= ((d1-d2).seconds)/3600.0  
                 diffdias= (d1-d2).days
 		
 		if diffdias > 0:
 			d2 = d1
-		elif diffdia = 0:
+		elif diffdia2 = 0:
 			if diffhora > 0:
 				d2 = d1
 			
