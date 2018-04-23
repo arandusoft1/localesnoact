3fa598f71249ab3be1321b64efb764bc1de7b951
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-23 18:37:15 -0300

    prueba solo ultact

--- app.py
+++ app.py
@@ -24,16 +24,17 @@ def index():
 	
 	for row in rows:
 		empresas.append({"Empresa": row[1],"Sucursal": row[2],"fVigencia": row[3],"CantPrecio": row[4]})
-		d1 = datetime.strptime(row[3],fmt)		
-		diffhora= ((d1-d2).seconds)/3600.0  
-                diffdias= (d1-d2).days		
-		if diffdias > 0:
-			d2 = d1
-			ultact = row[3]
-		elif diffdia2 = 0:
-			if diffhora > 0:
-				d2 = d1
-				ultact = row[3]
+		#d1 = datetime.strptime(row[3],fmt)		
+		#diffhora= ((d1-d2).seconds)/3600.0  
+                #diffdias= (d1-d2).days		
+		#if diffdias > 0:
+		#	d2 = d1
+		#	ultact = row[3]
+		#elif diffdia2 = 0:
+		#	if diffhora > 0:
+		#		d2 = d1
+		#		ultact = row[3]
+		
 				
 			
 
