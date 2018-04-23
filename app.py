147d4af577633210a34b519f18d36956ae5c808c
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-23 12:51:21 -0300

    agrege coma

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ bottle.debug(True)
 def index():
 	conn = psycopg2.connect(database='d3fkm1msg7kiub',user='wdtetudvoejjev',password='b7fefda1a504e80018b763ba3d8bcb94804c54dfff9a3372b4a70ee042dadf22', host='ec2-54-83-1-94.compute-1.amazonaws.com')
 	con = conn.cursor()
-	con.execute("select * from Empresas")
+	con.execute("select * from Empresas;")
 	rows = con.fetchall()
 	#empresas= []
 	
