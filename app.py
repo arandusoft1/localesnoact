59a66d0ddbe0e71547f362e62bca694d40b871a5
arandusoft1 <38497767+arandusoft1@users.noreply.github.com>
2018-04-23 12:31:44 -0300

    prueba para ver si lee sql

--- app.py
+++ app.py
@@ -19,14 +19,13 @@ def index():
 	empresas= []
 	
 	for row in rows:
-		empresas.append({"Empresa": row[1],"Sucursal": row[2],"fVigencia": row[3],"CantPrecio": row[4]})
-		
-	print empresas
+		# empresas.append({"Empresa": row[1],"Sucursal": row[2],"fVigencia": row[3],"CantPrecio": row[4]})
+		print rows
 	
-	leer = {"Empresas": [ empresas ], "UltAct": [{"fVigencia": "05/03/18 00:00:00"}]}
+	#leer = {"Empresas": [ empresas ], "UltAct": [{"fVigencia": "05/03/18 00:00:00"}]}
 	
 		
-	# leer = json.loads(open('locales.json').read())	
+	leer = json.loads(open('locales.json').read())	
 	return template('tabla.tpl', leer)
 
 if __name__ == '__main__':
